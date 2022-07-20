56c780f4507403fb8cb4f88a281c6ddd51ab6669
Your Name <you@example.com>
2022-07-20 15:45:47 +0700

    add sld

--- js/slider.js
+++ js/slider.js
@@ -18,7 +18,8 @@ tabs.forEach((tab,index)=>{
   // console.log([tab.classList.value])
 
   tab.onclick= function(){
-    
+    clearInterval(timeout)
+    auto(index);
     // console.log(index); 
     $(".tab-pane.active").classList.remove("active");
     $(".tab-item.active").classList.remove("active");
@@ -29,7 +30,7 @@ tabs.forEach((tab,index)=>{
     line.style.left = this.offsetLeft + "px";
     line.style.width = this.offsetWidth + "px";
 
-
+ 
     
   
   }
@@ -40,59 +41,25 @@ tabs.forEach((tab,index)=>{
 
 
 
-var index=0,time = 3000 ;
-// console.log(tabs[3])
-setInterval( () => {
+var index=0,time = 1000 ;
+var timeout;
+function auto(index){
+  timeout=setInterval( () => {
   
-  tabs[index].classList.remove('active');
-  pane[index].classList.remove('active');
-  index++;
-  if (index === tabs.length) index = 0;
+    tabs[index].classList.remove('active');
+    pane[index].classList.remove('active');
+    index++;
+    if (index === tabs.length) index = 0;
+  
+    tabs[index].classList.add('active');
+    pane[index].classList.add('active');
+  
+    line.style.left = tabs[index].offsetLeft + "px";
+    line.style.width = tabs[index].offsetWidth + "px";
+  
+  }, time);
+  
+}
 
-  tabs[index].classList.add('active');
-  pane[index].classList.add('active');
+auto(index);
 
-  line.style.left = tabs[index].offsetLeft + "px";
-  line.style.width = tabs[index].offsetWidth + "px";
-
-}, time);
-
-
-
-
-
-
-
-
-//console.log(slinext);
-
-
-
-// const tabs = $$(".tab-item");
-// const panes = $$(".tab-pane");
-
-
-
-// const tabActive = $(".tab-item.active");
-// const line = $(".tabs .line");
-
-
-// requestIdleCallback(function () {
-//   line.style.left = tabActive.offsetLeft + "px";
-//   line.style.width = tabActive.offsetWidth + "px";
-// });
-
-// tabs.forEach((tab, index) => {
-//   const pane = panes[index];
-
-//   tab.onclick = function () {
-//     $(".tab-item.active").classList.remove("active");
-//     $(".tab-pane.active").classList.remove("active");
-
-//     line.style.left = this.offsetLeft + "px";
-//     line.style.width = this.offsetWidth + "px";
-
-//     this.classList.add("active");
-//     pane.classList.add("active");
-//   };
-// });
